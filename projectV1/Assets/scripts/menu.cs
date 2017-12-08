b809fbf5ed698a43384761ca603a6a0a6d567f89
RichardMcDyer <richardmcdyer@gmail.com>
2017-12-08 12:02:34 +0000

    leaderboard

--- projectV1/Assets/scripts/menu.cs
+++ projectV1/Assets/scripts/menu.cs
@@ -7,6 +7,7 @@ public class menu : MonoBehaviour {
 	public Button StartBtn;
 	public Button ExitBtn;
 	public Button BonusBtn;
+	public Button LeadBtn;
 	// Use this for initialization
 	void Start () {
 		StartBtn.onClick.AddListener (() => {
@@ -15,6 +16,9 @@ public class menu : MonoBehaviour {
 		ExitBtn.onClick.AddListener (() => {
 			Application.Quit();
 		});
+		/*LeadBtn.onClick.AddListener (() => {
+			SceneManager.LoadScene("LeaderBoard");
+		});*/
 		BonusBtn.onClick.AddListener (() => {
 			SceneManager.LoadScene ("race_track_lake");
 		});
